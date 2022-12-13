076c37a275c9d2aec58bac3f77cc24d0a41b0a88
yashinshekh <developeryasin@gmail.com>
2022-12-13 11:12:46 +0545

    asf

--- main/python/Browser.py
+++ main/python/Browser.py
@@ -1,207 +1,62 @@
-from PyQt5.QtCore import QThread, pyqtSignal, QEventLoop, QTimer
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
-driver_options = webdriver.ChromeOptions()
-import os
-import time
-from parsel import Selector
-import platform
+from PyQt5.QtCore import QThread, pyqtSignal
 import datetime
 from datetime import timedelta
-import csv
+from selenium import webdriver
+import platform
+import os
+import re
+from parsel import Selector
+from selenium.webdriver.firefox.options import Options
+import time
+
 
 class Browser(QThread):
     signal = pyqtSignal('PyQt_PyObject')
 
-    def __init__(self,asins,domain,sleeptime):
+    def __init__(self,searchterm):
         QThread.__init__(self)
-        self.asins = asins
-        self.domain = domain
-        self.sleeptime = sleeptime
+        self.expirydate = '2023/04/01'
+        self.searchterm = searchterm
 
-        print(self.asins)
+    def getdata(self,url):
+        self.driver.get(url)
+
+        alltexts = ''.join([i.strip() for i in Selector(text=self.driver.page_source).xpath('.//text()').extract() if i.strip()])
+        emails = list(set([i.lower() for i in re.findall(r'[\w\.-]+@[\w\.-]+', alltexts) if '.png' not in str(i) and 'wixpress' not in str(i)
+                                    and '@1' not in str(i) and '@2' not in str(i) and '@2' not in str(i) and '@3' not in str(i) and '@4' not in str(i)
+                                    and '@5' not in str(i) and '@6' not in str(i) and '@7' not in str(i) and '@8' not in str(i) and '@9' not in str(i)
+                                    and '.x' not in str(i) and '.xn' not in str(i) and 'nr@' not in str(i) and '@n0' not in str(i) and '.content' not in str(i)
+                                    and 'name@' not in str(i) and 'email@' not in str(i) and 'content@' not in str(i) and 'filterempty' not in str(i)
+                                    and 'striphtml' not in str(i) and '.0' not in str(i) and 'domain' not in str(i) and 'example' not in str(i) and '.n02' not in str(i)
+                                    and 'jpeg' not in str(i) and 'jpg' not in i and 'png' not in i and 'sentry' not in i and 'svg' not in str(i) and '@0' not in str(i)
+                                    and '.js' not in str(i) and '@.' not in i and '.@' not in i and 'email' not in i and 'media' not in i and '-' not in i
+                                    ]))
+
+        for email in emails:
+            if '.com' in email:
+                email = email[:email.index('.com')+4]
+                self.signal.emit([email])
+                print([email])
+
+
+        nextlink = Selector(text=self.driver.page_source).xpath('.//*[@id="pnnext"]/@href').extract_first()
+        if nextlink:
+            time.sleep(10)
+            self.getdata("https://www.google.com"+nextlink)
+
 
 
     def run(self):
-        options = webdriver.ChromeOptions()
+        options = Options()
+        # options.add_argument('--headless')
 
         if platform.system() == "Windows":
-            newpath = os.getenv('LOCALAPPDATA')+"\\Google\\Chrome\\User Data\\Selenium Profile"
-            options.add_argument("--user-data-dir="+newpath)
-
-            self.driver = webdriver.Chrome(
-                executable_path=os.getcwd()+'\\chromedriver.exe',
-                options=options
-            )
-
+            self.driver = webdriver.Firefox(executable_path=os.getcwd()+'\\geckodriver.exe',options=options)
         else:
-            options.add_argument("user-data-dir=~/.config/chromium")
-            self.driver = webdriver.Chrome(
-                executable_path=os.getcwd()+'/chromedriver',
-                options=options
-            )
+            self.driver = webdriver.Firefox(options=options)
 
-        self.driver.get(self.domain)
-        time.sleep(int(self.sleeptime))
-        time.sleep(5)
-        # self.driver.get("https://www.amazon.com/dp/B076PSBJX7")
+        if datetime.datetime.strptime(self.expirydate,'%Y/%m/%d')+timedelta(days=46) >= datetime.datetime.now():
 
-        jse,jsp = '',''
-        he,hp = '',''
-        ke,kp = '',''
-        se,sp = '',''
-        oe,op = '',''
-
-        with open("credentials.csv","r") as r:
-            reader = csv.reader(r)
-            for line in reader:
-                if line[0] == "junglescout username":
-                    jse = line[1]
-                if line[0] == "junglescount password":
-                    jsp = line[1]
-                if line[0] == "helium username":
-                    he = line[1]
-                if line[0] == "helium password":
-                    hp = line[1]
-                if line[0] == "keepa username":
-                    ke = line[1]
-                if line[0] == "keepa password":
-                    kp = line[1]
-                if line[0] == "scanunlimited username":
-                    se = line[1]
-                if line[0] == "scanunlimited password":
-                    sp = line[1]
-                if line[0] == "onlineseller username":
-                    oe = line[1]
-                if line[0] == "onlineseller password":
-                    op = line[1]
-
-        try:
-            self.driver.get("https://app.scanunlimited.com/session/signin")
-            time.sleep(2)
-            self.driver.find_element_by_id("mat-input-0").send_keys(se)
-            time.sleep(2)
-            self.driver.find_element_by_id("mat-input-1").send_keys(sp)
-            time.sleep(2)
-            self.driver.find_element_by_xpath('.//div/text()[contains(.,"Login")]/..').click()
-            time.sleep(5)
-
-        except:
-            pass
-
-        try:
-            self.driver.get("https://login.junglescout.com/")
-            time.sleep(2)
-            self.driver.find_element_by_id("email").send_keys(jse)
-            time.sleep(1)
-            self.driver.find_element_by_id("current-password").send_keys(jsp)
-            time.sleep(1)
-            self.driver.find_element_by_xpath('.//button[contains(.,"Log in")]').click()
-            time.sleep(3)
-        except:
-            pass
-
-        try:
-            self.driver.get("https://members.helium10.com/user/signin")
-            time.sleep(2)
-            self.driver.find_element_by_id("loginform-email").send_keys(he)
-            time.sleep(1)
-            self.driver.find_element_by_id("loginform-password").send_keys(hp)
-            time.sleep(2)
-            self.driver.find_element_by_xpath('.//button[contains(.,"Log In")]').click()
-            time.sleep(3)
-
-        except:
-            pass
-
-        try:
-            self.driver.get("https://keepa.com/#!manage")
-            time.sleep(15)
-            self.driver.find_element_by_id("panelUserRegisterLogin").click()
-            time.sleep(2)
-            self.driver.find_element_by_id("submitLogin").click()
-            time.sleep(2)
-            self.driver.find_element_by_id("username").send_keys(ke)
-            time.sleep(1)
-            self.driver.find_element_by_id("password").send_keys(kp)
-            time.sleep(1)
-            self.driver.find_element_by_id("submitLogin").click()
-            time.sleep(2)
-        except:
-            pass
-
-
-        for asin in self.asins:
-            print(self.domain+"/dp/"+str(asin))
-            self.driver.get(self.domain+"/dp/"+str(asin))
-            time.sleep(10)
-
-            try:
-                scan_unlimited_danger = "No Danger" if 'help_outline-24px.svg' in  self.driver.find_element_by_xpath('.//*[@id="ipwarning"]/@src').text else "Danger"
-            except:
-                scan_unlimited_danger = ''
-            try:
-                brand = self.driver.find_element_by_xpath('.//a[contains(.,"Brand:")]').text.replace("Brand:",'').split()[0]
-            except:
-                brand = ''
-            try:
-                monthly_sale_js = self.driver.find_element_by_xpath('.//label[contains(.,"Monthly Sales")]/../following-sibling::label').text
-            except:
-                monthly_sale_js = ''
-            try:
-                monthly_sale_helium = self.driver.find_element_by_xpath('.//div[@class="sc-jWESwd fmIRyj"][contains(.,"30-Day Sales")]/following-sibling::div').text
-            except:
-                monthly_sale_js,monthly_sale_helium = '',''
-
-            print(monthly_sale_js)
-            print(monthly_sale_helium)
-
-
-            ActionChains(self.driver).send_keys(Keys.SPACE).perform()
-            ActionChains(self.driver).send_keys(Keys.SPACE).perform()
-
-            try:
-                self.driver.switch_to.frame(self.driver.find_element_by_xpath('.//*[@id="keepa"]'))
-                time.sleep(1)
-                self.driver.find_element_by_xpath('.//li[contains(.,"Data")]').click()
-                time.sleep(1)
-                self.driver.find_element_by_xpath('.//li[contains(.,"Buy Box Statistics")]').click()
-                time.sleep(7)
-                self.driver.find_element_by_xpath('.//span[@data-range="180"]').click()
-                time.sleep(7)
-
-                keepa_stock = self.driver.find_element_by_xpath('.//*[@class="ag-center-cols-container"]/div/div[4]/span').text
-            except:
-                keepa_stock = ''
-            print("keepa stock: "+str(keepa_stock))
-
-            self.driver.switch_to.default_content()
-
-            # driver.find_element_by_id("scxt-stock-btn").click()
-            try:
-                self.driver.execute_script("document.getElementById('scxt-stock-btn').click()")
-                time.sleep(7)
-                self.driver.switch_to.frame(self.driver.find_element_by_xpath('.//*[@id="scxt-widget"]/iframe'))
-
-                brands = Selector(text=self.driver.page_source).xpath('.//*[@id="results"]//tr/td[1]/a/text()').extract()
-
-                os_fba = "NO"
-                for b in brands:
-
-                    if brand in b:
-                        os_fba = 'YES'
-                        break
-
-
-
-                # os_fba = self.driver.find_element_by_xpath('.//div/text()[contains(.,"FBA")]/../following-sibling::div').text
-            except:
-                os_fba = ''
-            print("os fba : "+str(os_fba))
-            self.driver.switch_to.default_content()
-
-            self.signal.emit([asin,monthly_sale_js,monthly_sale_helium,keepa_stock,scan_unlimited_danger,os_fba])
-            print([asin,monthly_sale_js,monthly_sale_helium,keepa_stock,scan_unlimited_danger,os_fba])
+            self.getdata('https://www.google.com/search?q='+self.searchterm)
 
         self.driver.close()
--- main/python/main.py
+++ main/python/main.py
@@ -1,55 +1,36 @@
-# -*- coding: utf-8 -*-
 from PyQt5.uic.properties import QtWidgets
+from PyQt5 import QtCore, QtWidgets
+import requests
+from PyQt5.QtCore import QThread, pyqtSignal, QEventLoop, QTimer
 from PyQt5.QtWidgets import QFileDialog
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from parsel import Selector
 import csv
-from PyQt5 import QtCore, QtGui, QtWidgets
-from selenium import webdriver
+from datetime import datetime, timedelta
 from Browser import Browser
-import os
-import platform
-import wget
-
-if platform.system() == "Windows":
-    import pkg_resources.py2_warn
-
-# from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.options import Options
 
 
-class Ui_Form(QtWidgets.QMainWindow):
+class Ui_Dialog(QtWidgets.QMainWindow):
     def setupUi(self, widget):
         widget.setObjectName("widget")
-        widget.resize(361, 406)
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(widget)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        widget.resize(352, 362)
+        self.gridLayout = QtWidgets.QGridLayout(widget)
+        self.gridLayout.setObjectName("gridLayout")
         self.tabWidget = QtWidgets.QTabWidget(widget)
         self.tabWidget.setObjectName("tabWidget")
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.tab)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.tab)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.verticalLayout.addLayout(self.horizontalLayout)
         self.lineEdit = QtWidgets.QLineEdit(self.tab)
         self.lineEdit.setObjectName("lineEdit")
         self.verticalLayout.addWidget(self.lineEdit)
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.verticalLayout.addWidget(self.lineEdit_2)
-        self.pushButton = QtWidgets.QPushButton(self.tab)
-        self.pushButton.setObjectName("pushButton")
-        self.verticalLayout.addWidget(self.pushButton)
         self.textBrowser = QtWidgets.QTextBrowser(self.tab)
         self.textBrowser.setObjectName("textBrowser")
         self.verticalLayout.addWidget(self.textBrowser)
-        self.pushButton_2 = QtWidgets.QPushButton(self.tab)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.verticalLayout.addWidget(self.pushButton_2)
-        self.horizontalLayout_2.addLayout(self.verticalLayout)
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
@@ -58,11 +39,8 @@ class Ui_Form(QtWidgets.QMainWindow):
         self.label_2 = QtWidgets.QLabel(self.tab_2)
         self.label_2.setObjectName("label_2")
         self.verticalLayout_3.addWidget(self.label_2)
-        self.label = QtWidgets.QLabel(self.tab_2)
-        self.label.setObjectName("label")
-        self.verticalLayout_3.addWidget(self.label)
         self.tabWidget.addTab(self.tab_2, "")
-        self.verticalLayout_2.addWidget(self.tabWidget)
+        self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
 
         self.retranslateUi(widget)
         self.tabWidget.setCurrentIndex(0)
@@ -70,79 +48,45 @@ class Ui_Form(QtWidgets.QMainWindow):
 
     def retranslateUi(self, widget):
         _translate = QtCore.QCoreApplication.translate
-        widget.setWindowTitle(_translate("widget", "AMAZON SCRAPER"))
-        self.lineEdit.setPlaceholderText(_translate("widget", "AMAZON domain (http://amazon.com/)"))
-        self.lineEdit_2.setPlaceholderText(_translate("widget", "Waiting Delay (s)"))
-        self.pushButton.setText(_translate("widget", "*.CSV Sku"))
-        self.pushButton_2.setText(_translate("widget", "Start Automation "))
+        widget.setWindowTitle(_translate("widget", "Google Email Scraper"))
+        self.lineEdit.setPlaceholderText(_translate("widget", "Google Search Term"))
+        self.pushButton.setText(_translate("widget", "Start Scraping"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("widget", "Main"))
         self.label_2.setText(_translate("widget", "https://fiverr.com/ajmiranisha"))
-        self.label.setText(_translate("widget", "RUN AS ADMINISTRATOR"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("widget", "Contact"))
 
-
+        self.pushButton.clicked.connect(self.start)
         self.threads = []
-        self.pushButton.clicked.connect(self.getfilename)
-        self.pushButton_2.clicked.connect(self.start)
-        self.fileName = "test"
-        self.profiles = []
-
-
-    def getfilename(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        self.fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getSaveFileName()","","CSV Files (*);;CSV Files (*.csv)", options=options)
-
-        try:
-            with open(self.fileName,"r") as r:
-                reader = csv.reader(r)
-                for line in reader:
-                    self.profiles.append(line[0])
-                    self.textBrowser.append(line[0])
-
-        except:
-            self.eliminate = []
 
 
     def start(self):
-        self.textBrowser.append("Started web scraping ...")
-
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        self.outputFile, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","CSV Files (*);;CSV Files (*.csv)", options=options)
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","CSV Files (*);;CSV Files (*.csv)", options=options)
+        if fileName:
+            self.textBrowser.append("Started web scraping ....")
 
-        self.outputFile = self.outputFile+'.csv' if self.outputFile else ''
+            loop = QEventLoop()
+            QTimer.singleShot(1000, loop.quit)
+            loop.exec_()
 
-        if self.outputFile:
-            with open(self.outputFile,"w",newline="",encoding="utf-8") as f:
+            self.filename = fileName + '.csv'
+            with open(self.filename,"w",newline="",encoding="utf-8") as f:
                 writer = csv.writer(f)
-                writer.writerow(['asin','monthly_sale_js','monthly_sale_helium','keepa_stock','scanunlimited','Has the brand owner or amazon made sales in the last 90 days?'])
+                writer.writerow(['email'])
 
 
-            self.textBrowser.append("Starting chrome browser ...")
-
-            newlists = self.profiles
-            # print(newlists)
-
-            if newlists and self.lineEdit.text() and self.lineEdit_2.text():
-                self.browser = Browser(newlists,self.lineEdit.text(),self.lineEdit_2.text())
-
-                self.threads.append(self.browser)
-                self.browser.start()
-                self.browser.signal.connect(self.trackdata)
-
-            else:
-                self.textBrowser.append("Missing values")
+            self.browser = Browser(self.lineEdit.text())
+            self.threads.append(self.browser)
+            self.browser.start()
+            self.browser.signal.connect(self.finished)
 
 
-    def trackdata(self,result):
+    def finished(self, result):
         self.textBrowser.append(result[0])
-        with open(self.outputFile,"a",newline="",encoding="utf-8") as f:
+        with open(self.filename,"a",newline="",encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerow(result)
-            print(result)
-
-
 
 
 
@@ -151,7 +95,7 @@ class AppContext(ApplicationContext):
         import sys
         app = QtWidgets.QApplication(sys.argv)
         Dialog = QtWidgets.QDialog()
-        ui = Ui_Form()
+        ui = Ui_Dialog()
         ui.setupUi(Dialog)
         Dialog.show()
         sys.exit(app.exec_())
